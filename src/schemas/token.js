a3bd3dfb34fa7282f4c7fd0318de288922eb965a
Bluebird2000 <ahmad.saka@photizzo.ng>
2019-08-29 18:33:33 +0100

    #delete untracked files

--- src/schemas/token.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var mongoose_1 = require("mongoose");
-exports.tokenSchema = new mongoose_1.Schema({
-    _userId: { type: mongoose_1.Schema.Types.ObjectId, required: true, ref: 'Register' },
-    token: { type: String, required: true },
-    createdAt: { type: Date, required: true, "default": Date.now, expires: 43200 }
-});
