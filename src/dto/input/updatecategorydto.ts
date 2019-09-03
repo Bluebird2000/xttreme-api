7b6f6f20e41de2d053e3079eeb977357a4bd9c95
Bluebird2000 <ahmad.saka@photizzo.ng>
2019-09-03 16:36:58 +0100

    add update category detail by id endpoint to app

---
+++ src/dto/input/updatecategorydto.ts
@@ -0,0 +1,26 @@
+import { Length, IsArray, IsNotEmpty, IsOptional } from "class-validator";
+
+export class UpdateCategoryDTO {
+  @IsNotEmpty({
+    message: "name is required"
+  })
+  @Length(4, 30, {
+    message: "name should be between 4 and 30 characters"
+  })
+  name: string;
+
+  @IsOptional()
+  @Length(3, 150, {
+    message: "description should be between 3 and 150 characters"
+  })
+  description: string;
+
+  constructor(
+    name?: string,
+    description?: string,
+    active?: boolean
+  ) {
+    this.name = name;
+    this.description = description;
+  }
+}
